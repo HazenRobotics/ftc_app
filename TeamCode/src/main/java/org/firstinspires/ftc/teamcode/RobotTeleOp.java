dde773aee7acc76510f71ac4b17949ad90c87a7a
HazenRobotics <31747681+HazenRobotics@users.noreply.github.com>
2017-09-25 22:57:19 -0700

    Made TeleOp Framework Class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotTeleOp.java
@@ -0,0 +1,79 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import java.util.ArrayList;
+
+/**
+ * Created by Alex on 9/23/2017.
+ */
+
+@TeleOp(name="TeleOp", group="TeleOp")
+@Disabled
+public class RobotTeleOp extends LinearOpMode {
+
+    //Add all global objects and lists
+    protected ArrayList<Toggle> ToggleList = new ArrayList<Toggle>();
+
+
+    //Add Motors, Servos, Sensors, etc here
+    //EX: protected DcMotor motor;
+
+
+    //Add all Constants here
+    //EX: protected final double MOTOR_POWER = 0.5;
+
+    @Override
+    public void runOpMode() {
+
+        setupHardware();
+        setupToggleList();
+        //Add any further initialization (methods) here
+
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            toggleLogic();
+
+            //Add any non-toggles here
+
+
+            telemetry.update();
+            idle();
+        }
+    }
+
+    protected void setupHardware() {
+        //Initializes the motor/servo variables here
+        /*EX:
+        motor = hardwareMap.dcMotor.get("motor");
+        motor.setDirection(DcMotor.Direction.FORWARD);*/
+
+
+    }
+
+    protected void setupToggleList() {
+        //Add any mechanics that can be controlled by a toggle here
+        /*EX:
+        ToggleList.add(new Toggle() {
+            protected boolean input() {return gamepad1.a;}
+            protected void turnOn() {motor.setPower(MOTOR_POWER);}
+            protected void turnOff() {motor.setPower(0);}
+            protected void debug() {telemetry.addData("Motor", "On: %b, Power: %.2f", isOn(), (isOn() ? MOTOR_POWER : 0.0));}
+        })*/
+
+
+    }
+
+    protected void toggleLogic() {
+        for(Toggle t: ToggleList) {
+            t.logic();
+            t.debug();
+        }
+    }
+
+    //Add new methods for functionality down here
+    
+}
