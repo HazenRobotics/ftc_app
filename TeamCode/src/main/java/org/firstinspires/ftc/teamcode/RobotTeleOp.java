3399cd3ef892567238b02d87cb2867ba97fc9918
HazenRobotics <31747681+HazenRobotics@users.noreply.github.com>
2017-10-04 22:31:12 -0700

    Transfered Code from ExtendRelic.java
    Cleaned up some of code additionally

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotTeleOp.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import java.util.ArrayList;
@@ -22,13 +23,14 @@ public class RobotTeleOp extends LinearOpMode {
 
     //Add Motors, Servos, Sensors, etc here
     //EX: protected DcMotor motor;
-    protected DcMotor mainLift;
-    protected Servo smallLift;
+    private Servo claw;
+    private DcMotor arm;
 
     //Add all Constants here
     //EX: protected final double MOTOR_POWER = 0.5;
-    protected final double SMALL_LIFT_LOWER_POS = 0.0, SMALL_LIFT_UPPER_POS = 0.0;
-    protected final double GLYPH_HEIGHT = 0.0;
+    private final double CLAW_POSITION_ONE = 0.0;
+    private final double CLAW_POSITION_TWO = 0.5;
+    private final double ARM_POWER = 0.4;
 
     @Override
     public void runOpMode() {
@@ -36,63 +38,16 @@ public class RobotTeleOp extends LinearOpMode {
         setupHardware();
         setupToggleList();
         //Add any further initialization (methods) here
-
-        double currentPosition = 0.0, nextPosition = 0.0;
-        int glyphOne  , glyphTwo  , glyphThree  , glyphFour  ,glyphZero; //glyphZero is the ground
-
+        
         waitForStart();
 
         while (opModeIsActive()) {
             toggleLogic();
 
             //Add any non-toggles here
-
-            //Main Lift Power
-            if((gamepad2.right_trigger>0) && (gamepad2.left_trigger==0)){
-
-                mainLift.setPower(gamepad2.right_trigger);
-            }
-            else if((gamepad2.right_trigger==0) && (gamepad2.left_trigger>0)){
-
-                mainLift.setPower(-(gamepad2.left_trigger));
-            }
-            else {
-                mainLift.setPower(0.0);
-            }
-
-            //Small Lift Power
-            if (gamepad2.right_bumper == true){
-                smallLift.setPosition(SMALL_LIFT_LOWER_POS);
-            }
-            else if (gamepad2.left_bumper == true){
-                smallLift.setPosition(SMALL_LIFT_UPPER_POS);
-            }
-
-            //nextPosition
-            /*if ((currentPosition < GLYPH_HEIGHT * 0) && (currentPosition > GLYPH_HEIGHT * 0)) {
-                nextPosition = glyphOne;
-            }
-            else if ((currentPosition < GLYPH_HEIGHT * 0) && (currentPosition > GLYPH_HEIGHT * 0)) {
-                nextPosition = glyphTwo;
-            }
-            else if ((currentPosition < GLYPH_HEIGHT * 0) && (currentPosition > GLYPH_HEIGHT * 0)) {
-                nextPosition = glyphThree;
-            }
-            else if ((currentPosition < GLYPH_HEIGHT * 0) && (currentPosition > GLYPH_HEIGHT * 0)) {
-                nextPosition = glyphFour;
-            }
-
-            //D Pad used to control Main Lift
-            if((gamepad2.dpad_up == false) && (gamepad2.dpad_down == false)) {
-                DPadMoving = false;
-            }
-
-
-            if(gamepad2.dpad_up == true) {
-                DPadMoving = true
-                mainLift.setPower(nextPosition-currentPosition);
-            }*/
-
+            
+            //Arm extension part
+            armExtension();
 
             telemetry.update();
             idle();
@@ -104,7 +59,10 @@ public class RobotTeleOp extends LinearOpMode {
         /*EX:
         motor = hardwareMap.dcMotor.get("motor");
         motor.setDirection(DcMotor.Direction.FORWARD);*/
+        claw = hardwareMap.servo.get("claw");
 
+        arm = hardwareMap.dcMotor.get("arm");
+        arm.setDirection(DcMotor.Direction.FORWARD);
 
     }
 
@@ -117,9 +75,13 @@ public class RobotTeleOp extends LinearOpMode {
             protected void turnOff() {motor.setPower(0);}
             protected void debug() {telemetry.addData("Motor", "On: %b, Power: %.2f", isOn(), (isOn() ? MOTOR_POWER : 0.0));}
         })*/
-//
-
-
+        ToggleList.add(new Toggle() {
+            //The y button on gamepad1 will trigger our toggle
+            protected boolean input() {return gamepad1.y;}
+            protected void turnOn() {claw.setPosition(CLAW_POSITION);}
+            protected void turnOff() {claw.setPosition(CLAW_POSITION_TWO);}
+            protected void debug() {telemetry.addData("Claw", "On: %b, Position: %.2f", isOn(), (isOn() ? CLAW_POSITION : CLAW_POSITION_TWO));}
+        });
     }
 
     protected void toggleLogic() {
@@ -130,5 +92,19 @@ public class RobotTeleOp extends LinearOpMode {
     }
 
     //Add new methods for functionality down here
+    
+    protected void armExtension() {
+        //When up arrow pressed, arm moves forward.  When up arrow released, arm stops moving.
+            if(gamepad2.dpad_up == true)
+                arm.setPower(ARM_POWER);
+
+            else if(gamepad2.dpad_up == false)
+                arm.setPower(0.0);
+            //When down arrow pressed, arm retracts.  When down arrow released, arm stops moving
+            if(gamepad2.dpad_down == true)
+                arm.setPower(-ARM_POWER);
+            else if(gamepad2.dpad_down == false)
+                arm.setPower(0.0);
+    }
     
 }
