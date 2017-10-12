c95ae64486d5edbe5f2c22a5a3116749c2386b33
HazenRobotics <hazen2014>
2017-10-12 15:52:14 -0700

    Finished all autonomous functions. Need testing to find proper inputs for constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotionControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotionControl.java
@@ -14,10 +14,13 @@ public class MotionControl {
     protected DcMotor rightBack;
 
     //Declare Constants
+    //currently arbitrary numbers, need to change for actual robot
     protected static final double COUNTS_PER_REV = 1440.0;
     protected static final double WHEEL_DIAMETER = 4.0;
     protected static final double COUNTS_PER_INCH = (COUNTS_PER_REV / WHEEL_DIAMETER);
-    protected static final double BASE_WHEEL_POWER = 0.5;
+    protected static final double ROBOT_RADIUS = 1.0;
+    protected static final double ROBOT_TURNING_CIRCUMFERENCE = Math.PI * (2 * ROBOT_RADIUS);
+    protected static final double DRIVE_SPEED = 1.0;
 
     //Global Variables
 
@@ -38,15 +41,15 @@ public class MotionControl {
         double rightBackPower = (y + x) / scale;
 
         //TODO: Might need to scale these values
-        double leftFrontTarget = leftFrontPower * counts;
-        double rightFrontTarget = rightFrontPower * counts;
-        double leftBackTarget = leftBackPower * counts;
-        double rightBackTarget = rightBackPower * counts;
+        int leftFrontTarget = (int) (leftFrontPower * counts);
+        int rightFrontTarget = (int) (rightFrontPower * counts);
+        int leftBackTarget = (int) (leftBackPower * counts);
+        int rightBackTarget = (int) (rightBackPower * counts);
 
-        leftFront.setTargetPosition((int) leftFrontTarget);
-        leftBack.setTargetPosition((int) leftBackTarget);
-        rightFront.setTargetPosition((int) rightFrontTarget);
-        rightBack.setTargetPosition((int) rightBackTarget);
+        leftFront.setTargetPosition(leftFrontTarget);
+        leftBack.setTargetPosition(leftBackTarget);
+        rightFront.setTargetPosition(rightFrontTarget);
+        rightBack.setTargetPosition(rightBackTarget);
 
         leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -60,6 +63,7 @@ public class MotionControl {
 
         while(leftFront.isBusy() && leftBack.isBusy() && rightFront.isBusy() && rightBack.isBusy()) {
             //can be used to display messages on the phone through telemetry
+            //idle();
         }
 
         leftFront.setPower(0);
@@ -75,6 +79,61 @@ public class MotionControl {
 
     public void turn(double turnAngle)
     {
+        double turnDistance = (turnAngle / 360) * ROBOT_TURNING_CIRCUMFERENCE;
+        double counts = turnDistance * COUNTS_PER_INCH;
 
+        double leftFrontPower = 0;
+        double rightFrontPower = 0;
+        double leftBackPower = 0;
+        double rightBackPower = 0;
+
+        if(turnAngle > 0){ //checks to see if angle is positive or negative to determine if bot turns left or right
+            leftFrontPower = -DRIVE_SPEED;
+            leftBackPower = -DRIVE_SPEED;
+            rightFrontPower = DRIVE_SPEED;
+            rightBackPower = DRIVE_SPEED;
+        }
+        else{
+            leftFrontPower = DRIVE_SPEED;
+            leftBackPower = DRIVE_SPEED;
+            rightFrontPower = -DRIVE_SPEED;
+            rightBackPower = -DRIVE_SPEED;
+        }
+
+        //TODO: Might need to scale these values
+        int leftFrontTarget = (int) (leftFrontPower * counts);
+        int rightFrontTarget = (int) (rightFrontPower * counts);
+        int leftBackTarget = (int) (leftBackPower * counts);
+        int rightBackTarget = (int) (rightBackPower * counts);
+
+        leftFront.setTargetPosition(leftFrontTarget);
+        leftBack.setTargetPosition(leftBackTarget);
+        rightFront.setTargetPosition(rightFrontTarget);
+        rightBack.setTargetPosition(rightBackTarget);
+
+        leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        leftFront.setPower(leftFrontPower);
+        leftBack.setPower(leftBackPower);
+        rightFront.setPower(rightFrontPower);
+        rightBack.setPower(rightBackPower);
+
+        while(leftFront.isBusy() && leftBack.isBusy() && rightFront.isBusy() && rightBack.isBusy()) {
+            //can be used to display messages on the phone through telemetry
+            //idle();
+        }
+
+        leftFront.setPower(0);
+        leftBack.setPower(0);
+        rightFront.setPower(0);
+        rightBack.setPower(0);
+
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 }
