6d1b33c7839c1fcc42fd8f3db407dc4ede3da932
Robotics <hazenrobotics@gmail.com>
2017-10-21 09:02:27 -0700

    Commented claw autonomous function.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/interfaces/motors/ArmMotors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/interfaces/motors/ArmMotors.java
@@ -16,6 +16,7 @@ public class ArmMotors implements IArm {
     private DigitalChannel limitClosed;
     private final double CLAW_POWER = 0.2;
 
+    //initial setup, defines motor and limit switches.
     public ArmMotors(IHardware hardware) {
         this.hardware = hardware;
         this.claw = hardware.getMotor("claw");
@@ -25,16 +26,20 @@ public class ArmMotors implements IArm {
         limitClosed.setMode(DigitalChannel.Mode.INPUT);
     }
 
+    //when this method used, claw closes. (motor moves from starting position till claw closed on glyph,
+    // limit switch then activates and stops motor)
     @Override
     public void grabGlyph() {
-        claw.setPower(-CLAW_POWER);
+        claw.setPower(CLAW_POWER);
         while(!limitClosed.getState()) hardware.idle();
         claw.setPower(0);
     }
 
+    //when this method used, claw opens. (motor moves toward starting position,
+    // limit switch activates at initial position and stops motor)
     @Override
     public void dropGlyph() {
-        claw.setPower(CLAW_POWER);
+        claw.setPower(-CLAW_POWER);
         while(!limitOpen.getState()) hardware.idle();
         claw.setPower(0);
     }
