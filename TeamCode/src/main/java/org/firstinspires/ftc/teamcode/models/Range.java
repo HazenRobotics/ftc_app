1890cb3428f8fb183bf84b0915ea4674e1c74cd1
and.ngu011 <andynguyen2171@gmail.com>
2018-02-08 15:52:54 -0800

    added range javadoc

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/models/Range.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/models/Range.java
@@ -8,12 +8,22 @@ public class Range extends Condition {
     protected final I2cRangeSensor rangeSensor;
     protected final boolean moveGreater;
 
+    /**
+     * A condition to move until distance reached.
+     * @param distance target distance to move
+     * @param rangeSensor range sensor for distance to be read from
+     * @param moveGreater whether moving to greater distance
+     */
     public Range(float distance, I2cRangeSensor rangeSensor, boolean moveGreater) {
         this.distance = distance;
         this.rangeSensor = rangeSensor;
         this.moveGreater = moveGreater;
     }
 
+    /**
+     * Checks if distance is reached.
+     * @return true if reached target distance
+     */
     @Override
     public boolean isTrue() {
         double currentDistance = rangeSensor.readUltrasonic(DistanceUnit.INCH);
